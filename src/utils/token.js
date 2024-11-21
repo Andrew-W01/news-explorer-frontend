f6cf90c4ed762412fd7813625a83ed44e27f7ca4
Andrew.Worley <drewjw1212@gmail.com>
2024-11-21 18:52:08 -0500

    final project

--- src/utils/token.js
+++ src/utils/token.js
@@ -2,6 +2,6 @@ const TOKEN_KEY = "jwt";
 
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 
-export const setToken = () => localStorage.setItem(TOKEN_KEY, token);
+export const setToken = (token) => localStorage.setItem(TOKEN_KEY, token);
 
 export const removeToken = () => localStorage.removeToken(TOKEN_KEY);
