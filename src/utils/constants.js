c244a328f7bd517f8f2fe62684e041222fc2de52
Andrew.Worley <drewjw1212@gmail.com>
2024-11-24 18:23:00 -0500

    final project

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,5 +3,5 @@ export const APIkey = "e9dc6c26d0844614aca52547a8b36141";
 
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://aw-newsexplorer.wildsurf.net"
+    ? "https://andrew-w01.github.io/news-explorer-frontend/"
     : "http://localhost:3002";
