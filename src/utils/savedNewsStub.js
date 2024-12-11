9a11234555dbf94a7fb373edff71666ad44395d0
Andrew.Worley <drewjw1212@gmail.com>
2024-12-10 20:33:34 -0500

    final project

---
+++ src/utils/savedNewsStub.js
@@ -0,0 +1,37 @@
+export const savedNewsStub = [
+  {
+    source: { id: null, name: "[Removed]" },
+    author: null,
+    title: "[Removed]",
+    description: "[Removed]",
+    url: "https://removed.com",
+    urlToImage: null,
+    publishedAt: "2024-12-10T23:02:21Z",
+    content: "[Removed]",
+  },
+  {
+    source: { id: null, name: "Electrek" },
+    author: "Peter Johnson",
+    title:
+      "After buying an EV, less than 1% of drivers go back to gas-powered cars",
+    description:
+      "Once you go electric, you almost never go back to a gas-powered car...",
+    url: "http://electrek.co/2024/12/10/after-buying-an-ev-less-than-1-drivers-go-back-gas-cars/",
+    urlToImage:
+      "https://i0.wp.com/electrek.co/wp-content/uploads/sites/3/2024/12/EV-drivers-gas-cars-1.jpeg?resize=1200%2C628&quality=82&strip=all&ssl=1",
+    publishedAt: "2024-12-10T22:54:08Z",
+    content:
+      "Once you go electric, you almost never go back to a gas-powered car...",
+  },
+  {
+    source: { id: null, name: "Fark.com" },
+    author: null,
+    title: "Stellantis CEO forced out after making 'arrogant mistakes'...",
+    description:
+      "Stellantis CEO forced out after making 'arrogant mistakes' which is weird...",
+    url: "https://www.fark.com/comments/13489802/Stellantis-CEO-forced-out...",
+    urlToImage: "https://img.fark.net/images/2013/site/farkLogo2Big.gif",
+    publishedAt: "2024-12-10T22:50:09Z",
+    content: "Links are submitted by members of the Fark community...",
+  },
+];
